ee4c720d44e540df266c5401e007b2462fc7b99b
riamarquesx <rianmarques0106@gmail.com>
2025-10-13 22:30:39 -0300

    CSS

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DisneyList from "../components/DisneyList";
+
+
+export default function Home () {
+    return (
+        <section>
+            <h2> Lista de filmes </h2>
+            <DisneyList />        
+        </section>
+    );
+}
