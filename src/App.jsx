2585c94c70429dd47df4e5966921951144658a3a
riamarquesx <rianmarques0106@gmail.com>
2025-10-17 22:19:25 -0300

    css

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import Home from "./pages/Home";
 
 
+
 export default function App () {
     return (
       <div>
-      <h1>Filmes da Disney</h1>
       <Home/>
       </div>        
     );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import DisneyList from "../components/DisneyList";
 
 
+
 export default function Home () {
     return (
         <section>
